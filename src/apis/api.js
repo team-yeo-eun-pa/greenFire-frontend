7c4ccfa54768a85a84d073352474d426b666ee75
siso <116954896+sisology@users.noreply.github.com>
2024-05-28 15:39:55 +0900

    Feat: api request 수정

--- src/apis/api.js
+++ src/apis/api.js
@@ -64,7 +64,7 @@ export async function postRefreshToken() {
 
     return await request(
         'POST',
-        '/api/v1/refresh-token',
+        '/refresh-token',
         { 'Refresh-Token' : getRefreshTokenHeader() }
     );
 }
