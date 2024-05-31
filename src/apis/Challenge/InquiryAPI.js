954a60191ebeea268af432eeccd30178e5b00578
hohojeong091 <hj660833@gmail.com>
2024-05-31 15:06:14 +0900

    bug : 문의센터 메인페이지 접근 오류 해결

--- src/apis/Challenge/InquiryAPI.js
+++ src/apis/Challenge/InquiryAPI.js
@@ -1,6 +1,5 @@
-import async from "async";
+
 import {request} from "../api";
-import {success} from "../../modules/MemberModules";
 import {toast} from "react-toastify";
 import {getInquiry} from "../../modules/InquiryModules";
 
@@ -8,7 +7,8 @@ export const callInquiryListAPI = ({currentPage = 1}) => {
     return async (dispatch, getState) => {
         const result = await request(`GET`, `/member/inquiry?page=${currentPage}`);
 
-        if(result.status === 200) {
+        console.log("getState : ", getState)
+        if(result?.status === 200) {
             dispatch(getInquiry(result));
         } else {
             toast.warning("문의 목록 조회에 실패했습니다. ")
