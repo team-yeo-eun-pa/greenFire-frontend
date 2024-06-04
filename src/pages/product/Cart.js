871366ae0242c06240a9353535517fc6c51184a3
syyyl <lxxsxyxx@gmail.com>
2024-06-04 21:36:08 +0900

    #7 feat: cart items list, checkbox

--- src/pages/product/Cart.js
+++ src/pages/product/Cart.js
@@ -3,31 +3,33 @@ import {useState} from "react";
 
 
 const CheckboxGroup = Checkbox.Group;
-const plainOptions = ['상품1', '상품2', '상품3'];
+const cartList = ['상품1', '상품2', '상품3'];
 const defaultCheckedList = ['상품1'];
+
 
 
 function Cart() {
 
     const [checkedList, setCheckedList] = useState(defaultCheckedList);
-    const checkAll = plainOptions.length === checkedList.length;
-    const indeterminate = checkedList.length > 0 && checkedList.length < plainOptions.length;
+    const checkAll = cartList.length === checkedList.length;
+    const indeterminate = checkedList.length > 0 && checkedList.length < cartList.length;
     const onChangeCheck = (list) => {
         setCheckedList(list);
     };
     const onCheckAllChange = (e) => {
-        setCheckedList(e.target.checked ? plainOptions : []);
+        setCheckedList(e.target.checked ? cartList : []);
     };
 
     return (
         <>
-
             <Checkbox indeterminate={indeterminate} onChange={onCheckAllChange} checked={checkAll}>
                 전체선택
             </Checkbox>
             <Divider/>
             <div className="cart-list-wrapper">
-                <CheckboxGroup className="cart-checkbox" options={plainOptions} value={checkedList} onChange={onChangeCheck}/>
+                <CheckboxGroup className="cart-checkbox" options={cartList} value={checkedList} onChange={onChangeCheck}>
+
+                </CheckboxGroup>
             </div>
         </>
     )
