a65f4503e358cc5754e1e4470103d062f9293637
jeong-gayeon <1bfish106@naver.com>
2024-06-06 15:18:29 +0900

    [Feature] Member Notice View And Detail #63

--- src/apis/AdminNoticeAPICalls.js
+++ src/apis/AdminNoticeAPICalls.js
@@ -1,14 +1,26 @@
 
-import {authRequest} from "./api";
-import {getNotices} from "../modules/NoticeModules";
+import {authRequest, request} from "./api";
+import {getNotice, getNotices} from "../modules/NoticeModules";
 
-export const AdminNoticeAPICalls = ({currentPage = 1}) => {
+export const AdminNoticesAPICalls = ({currentPage = 1}) => {
     return async (dispatch, getState) => {
         const result = await authRequest.get(`/admin/notices?page=${currentPage}`);
-        console.log('AdminNoticeAPICalls result : ', result);
+        console.log('AdminNoticesAPICalls result : ', result);
 
         if(result.status === 200) {
             dispatch(getNotices(result));
+        }
+    }
+}
+
+export const AdminNoticeAPICalls = ({noticeCode}) => {
+    return async (dispatch, getState) => {
+        console.log("noticeCode api : ", noticeCode)
+        const result = await request('GET' , `/admin/notices/${noticeCode}`);
+        console.log('AdminNoticeAPICalls result : ', result);
+
+        if(result.status === 200) {
+            dispatch(getNotice(result));
         }
     }
 }
