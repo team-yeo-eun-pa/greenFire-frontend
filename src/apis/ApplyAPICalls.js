faa26d7b51e32ae05389dc4e941cbaf9aea35ea1
siso <116954896+sisology@users.noreply.github.com>
2024-06-09 23:40:58 +0900

    feat: Apply 수정 기능 구현

--- src/apis/ApplyAPICalls.js
+++ src/apis/ApplyAPICalls.js
@@ -45,26 +45,15 @@ export const callApplyRegistAPI = ({applyCreateRequest}) => {
 
 export const callApplyUpdateAPI = ({ sellerCode, applyRequest }) => {
     return async (dispatch, getState) => {
-        try {
-            const result = await request(
-                'PUT',
-                `/members/mypage/apply/modify/${sellerCode}`,
-                { 'Content-Type': 'application/json' },
-                JSON.stringify(applyRequest)
-            );
-
+            const result = await authRequest.put(`/members/mypage/apply/modify/${sellerCode}`, applyRequest);
             console.log('callApplyUpdateAPI result:', result);
 
-            if (result?.status === 200) {
+            if (result?.status === 201) {
                 dispatch(success());
                 toast.success("신청 정보가 성공적으로 업데이트되었습니다.");
             } else {
                 toast.warning("신청 정보 업데이트에 실패했습니다. 다시 시도해주세요.");
             }
-        } catch (error) {
-            console.error('Error updating apply info:', error);
-            toast.error("신청 정보 업데이트 중 오류가 발생했습니다. 다시 시도해주세요.");
-        }
     }
 }
 
--- src/pages/member/ApplyDetail.js
+++ src/pages/member/ApplyDetail.js
@@ -1,29 +1,25 @@
-import React, {useState, useEffect, useRef} from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams, useNavigate } from 'react-router-dom';
 import { Form, Button, Col, Row } from 'react-bootstrap';
 import { FaEdit } from 'react-icons/fa';
-import {callApplyCancelAPI, callApplyDetailAPI, callApplyRegistAPI, callApplyUpdateAPI} from "../../apis/ApplyAPICalls";
-import {FcCancel} from "react-icons/fc";
+import { FcCancel } from "react-icons/fc";
 import ApplyForm from "../../components/form/ApplyForm";
+import {
+    callApplyCancelAPI,
+    callApplyDetailAPI,
+    callApplyUpdateAPI
+} from "../../apis/ApplyAPICalls";
+import {toast} from "react-toastify";
 
 const ApplyDetail = () => {
     const dispatch = useDispatch();
     const navigate = useNavigate();
     const { sellerCode } = useParams();
-    const { applyDetail } = useSelector(state => state.applyReducer);
+    const { applyDetail, isLoading } = useSelector(state => state.applyReducer);
     const [isEditing, setIsEditing] = useState(false);
     const imageInput = useRef();
-
-    const [form, setForm] = useState({
-        storeName: '',
-        storeRepresentativeName: '',
-        businessNumber: '',
-        mosNumber: '',
-        memberPhone: 'test',
-        storeType: '',
-        applyContent: ''
-    });
+    const [form, setForm] = useState({});
 
     useEffect(() => {
         dispatch(callApplyDetailAPI({ sellerCode }));
@@ -42,187 +38,116 @@ const ApplyDetail = () => {
         }
     }, [applyDetail]);
 
-    const onClickApplyModifyHandler = () => {
-        try {
-            const formData = new FormData();
-            formData.append('businessImg', imageInput.current.files[0]);
-            formData.append('applyRequest', new Blob([JSON.stringify(form)], { type: 'application/json' }));
-
-            dispatch(callApplyUpdateAPI({ sellerCode, applyRequest: formData }));
-        } catch (error) {
-            console.error('Error updating application:', error);
-        }
-    }
-
-
-    const onClickCancelHandler = () => {
-        const formData = new FormData();
-        formData.append('cancelRequest', new Blob([JSON.stringify(form)], {type: 'application/json'}))
-        dispatch(callApplyCancelAPI({sellerCode, cancelRequest: formData}));
-    };
-
-    const onClickEditHandler = () => {
+    const handleEdit = () => {
         setIsEditing(true);
-        setForm({...applyDetail});
     };
 
-    const onClickBackHandler = () => {
+    const handleCancel = () => {
+        const formData = new FormData();
+        formData.append('cancelRequest', new Blob([JSON.stringify(form)], { type: 'application/json' }));
+        dispatch(callApplyCancelAPI({ sellerCode, cancelRequest: formData }));
+    };
+
+    const handleSave = () => {
+        const formData = new FormData();
+        formData.append('businessImg', imageInput.current.files[0]);
+        formData.append('applyRequest', new Blob([JSON.stringify(form)], { type: 'application/json' }));
+
+        dispatch(callApplyUpdateAPI({ sellerCode, applyRequest: formData }))
+            .then(() => {
+                toast.success("신청 정보가 성공적으로 업데이트되었습니다.");
+                setIsEditing(false);
+                navigate(`/members/mypage/apply`);
+            })
+            .catch(error => {
+                console.error('Error updating apply info:', error);
+                toast.error("신청 정보 업데이트 중 오류가 발생했습니다. 다시 시도해주세요.");
+            });
+    };
+
+    const handleBack = () => {
         setIsEditing(false);
     };
+
+    if (isLoading) {
+        return <p>로딩 중...</p>;
+    }
 
     return (
         <div className="mt-5">
             <Row className="justify-content-md-center">
                 <Col className="col-8">
-                <div className="w-100 mb-5 fs-4 fw-semibold border-bottom border-2 border-dark-subtle p-2">반딧불이 스토어 신청</div>
-                    {applyDetail ? (
+                    <div className="w-100 mb-5 fs-4 fw-semibold border-bottom border-2 border-dark-subtle p-2">반딧불이 스토어 신청</div>
+                    {applyDetail && (
                         isEditing ? (
-                            <Form>
+                            <div>
                                 <Col className="col-12">
                                     <ApplyForm apply={form} setForm={setForm} imageInput={imageInput} modifyMode={true} />
                                 </Col>
-                                <div
-                                    className="w-100 mb-1 fs-4 fw-semibold border-bottom border-2 border-dark-subtle p-2">
-                                </div>
+                                <div className="w-100 mb-1 fs-4 fw-semibold border-bottom border-2 border-dark-subtle p-2"></div>
                                 <div className="d-flex justify-content-end mt-3">
-                                    <Button variant="outline-secondary" className="mx-2" onClick={onClickBackHandler}>
+                                    <Button variant="outline-secondary" className="mx-2" onClick={handleBack}>
                                         취소
                                     </Button>
-                                    <Button variant="success" type="submit" className="mx-2" onClick={onClickApplyModifyHandler}>
+                                    <Button variant="success" className="mx-2" onClick={handleSave}>
                                         저장
                                     </Button>
                                 </div>
-                            </Form>
+                            </div>
                         ) : (
                             <div>
-                                {/*<Form.Group as={Row} className="mb-3" controlId="formStoreRepresentativeName">*/}
-                                {/*    <Form.Label column sm="3">*/}
-                                {/*        대표사진*/}
-                                {/*    </Form.Label>*/}
-                                {/*    <Col sm="9">*/}
-                                {/*        {imageUrl &&*/}
-                                {/*            <img*/}
-                                {/*                className="d-none"*/}
-                                {/*                alt="preview"*/}
-                                {/*                src={imageUrl}*/}
-                                {/*            />*/}
-                                {/*        }*/}
-                                {/*    </Col>*/}
-                                {/*</Form.Group>*/}
                                 <Form.Group as={Row} className="mb-3" controlId="formStoreName">
-                                <Form.Label column sm="3">
-                                        상호명
-                                    </Form.Label>
+                                    <Form.Label column sm="3">상호명</Form.Label>
                                     <Col sm="9">
-                                        <Form.Control
-                                            plaintext
-                                            readOnly
-                                            defaultValue={applyDetail.storeName}
-                                        />
+                                        <Form.Control plaintext readOnly defaultValue={applyDetail.storeName} />
                                     </Col>
                                 </Form.Group>
                                 <Form.Group as={Row} className="mb-3" controlId="formStoreRepresentativeName">
-                                    <Form.Label column sm="3">
-                                        대표자
-                                    </Form.Label>
+                                    <Form.Label column sm="3">대표자</Form.Label>
                                     <Col sm="9">
-                                        <Form.Control
-                                            plaintext
-                                            readOnly
-                                            defaultValue={applyDetail.storeRepresentativeName}
-                                        />
+                                        <Form.Control plaintext readOnly defaultValue={applyDetail.storeRepresentativeName} />
                                     </Col>
                                 </Form.Group>
                                 <Form.Group as={Row} className="mb-3" controlId="formBusinessNumber">
-                                    <Form.Label column sm="3">
-                                        사업자등록번호
-                                    </Form.Label>
+                                    <Form.Label column sm="3">사업자등록번호</Form.Label>
                                     <Col sm="9">
-                                        <Form.Control
-                                            plaintext
-                                            readOnly
-                                            defaultValue={applyDetail.businessNumber}
-                                        />
+                                        <Form.Control plaintext readOnly defaultValue={applyDetail.businessNumber} />
                                     </Col>
                                 </Form.Group>
                                 <Form.Group as={Row} className="mb-3" controlId="formMosNumber">
-                                    <Form.Label column sm="3">
-                                        통신판매업번호
-                                    </Form.Label>
+                                    <Form.Label column sm="3">통신판매업번호</Form.Label>
                                     <Col sm="9">
-                                        <Form.Control
-                                            plaintext
-                                            readOnly
-                                            defaultValue={applyDetail.mosNumber}
-                                        />
+                                        <Form.Control plaintext readOnly defaultValue={applyDetail.mosNumber} />
                                     </Col>
                                 </Form.Group>
-                                <Form.Group as={Row} className="mb-3" controlId="formStoreRepresentativeName">
-                                    <Form.Label column sm="3">
-                                        전문분야
-                                    </Form.Label>
+                                <Form.Group as={Row} className="mb-3" controlId="formStoreType">
+                                    <Form.Label column sm="3">전문분야</Form.Label>
                                     <Col sm="9">
-                                        <Form.Control
-                                            plaintext
-                                            readOnly
-                                            defaultValue={applyDetail.storeType}
-                                        />
+                                        <Form.Control plaintext readOnly defaultValue={applyDetail.storeType} />
                                     </Col>
                                 </Form.Group>
                                 <Form.Group as={Row} className="mb-3" controlId="formApplyContent">
-                                    <Form.Label column sm="3">
-                                        신청 한 마디
-                                    </Form.Label>
+                                    <Form.Label column sm="3">신청 한 마디</Form.Label>
                                     <Col sm="9">
-                                        <Form.Control
-                                            plaintext
-                                            readOnly
-                                            defaultValue={applyDetail.applyContent}
-                                        />
+                                        <Form.Control plaintext readOnly defaultValue={applyDetail.applyContent} />
                                     </Col>
                                 </Form.Group>
                                 <Form.Group as={Row} className="mb-3" controlId="formApplyDatetime">
-                                    <Form.Label column sm="3">
-                                        신청일
-                                    </Form.Label>
+                                    <Form.Label column sm="3">신청일</Form.Label>
                                     <Col sm="9">
-                                        <Form.Control
-                                            plaintext
-                                            readOnly
-                                            defaultValue={applyDetail.applyDatetime}
-                                        />
+                                        <Form.Control plaintext readOnly defaultValue={applyDetail.applyDatetime} />
                                     </Col>
                                 </Form.Group>
-                                {/*<Form.Group as={Row} className="mb-3" controlId="formApplyStatus">*/}
-                                {/*    <Form.Label column sm="3">*/}
-                                {/*        상태*/}
-                                {/*    </Form.Label>*/}
-                                {/*    <Col sm="9">*/}
-                                {/*        <Badge bg={getBadgeVariant(applyDetail.applyStatus)}>*/}
-                                {/*            {getBadgeText(applyDetail.applyStatus)}*/}
-                                {/*        </Badge>*/}
-                                {/*    </Col>*/}
-                                {/*</Form.Group>*/}
-                                {/*<Col sm="12">*/}
-                                    <div
-                                        className="w-100 mb-1 fs-4 fw-semibold border-bottom border-2 border-dark-subtle p-2">
-                                    </div>
-
-                                    <div className="d-flex justify-content-end mt-3">
-                                        <Button variant="outline-danger" className="btn-md mx-1"
-                                                onClick={onClickCancelHandler}>
-                                            <FcCancel/> 신청 취소
-                                        </Button>
-                                        <Button variant="outline-success" className="btn-md mx-1"
-                                                onClick={onClickEditHandler}>
-                                            <FaEdit/> 내용 편집
-                                        </Button>
-                                    </div>
-                                {/*</Col>*/}
+                                <div className="d-flex justify-content-end mt-3">
+                                    <Button variant="outline-danger" className="btn-md mx-1" onClick={handleCancel}>
+                                        <FcCancel /> 신청 취소
+                                    </Button>
+                                    <Button variant="outline-success" className="btn-md mx-1" onClick={handleEdit}>
+                                        <FaEdit /> 내용 편집
+                                    </Button>
+                                </div>
                             </div>
                         )
-                    ) : (
-                        <p>로딩 중...</p>
                     )}
                 </Col>
             </Row>
