f42fd90a1ff798f76a1f5af213dca14d5b981cde
siso <116954896+sisology@users.noreply.github.com>
2024-06-14 01:02:09 +0900

    fix: 사이드바 프로필 정보 제외

--- src/components/common/UserPageNavBar.js
+++ src/components/common/UserPageNavBar.js
@@ -6,51 +6,46 @@ import { NavLink } from "react-router-dom";
 import { CgProfile } from "react-icons/cg";
 import { Link } from 'react-router-dom';
 
-function UserPageNavBar({ profileInfo }) {
+function UserPageNavBar() {
     const [key, setKey] = useState('home');
-
-    // 프로필 정보가 존재하는지 확인하고
-    if (!profileInfo) {
-        return null; // 프로필 정보가 없을 경우 렌더링 안함 근데 시온이가 이걸 원한게 맞을까
-    }
-
-    // 프로필 정보에서 필요한 부분만 추출하여 전달
-    const { profilePicture, memberName, memberEmail } = profileInfo;
 
     return (
         <>
             <Nav defaultActiveKey="/home" className="flex-column rounded mt-5 p-4"
                  style={{backgroundColor: "#F0F3EE"}}>
                 {/* User Info */}
-                <div className="text-center mb-3">
-                    {profilePicture ? (
-                        <Image
-                            src={profilePicture}
-                            roundedCircle
-                            className="mx-auto d-block mb-3"
-                            style={{width: "130px", height: "130px"}}
-                        />
-                    ) : (
-                        <CgProfile style={{width: "130px", height: "130px"}} />
-                    )}
-                    <br/>
-                    <div className="fw-bold fs-6">
-                        {memberName}
-                    </div>
-                    <div className="text-muted">{memberEmail}</div>
-                    <NavLink to="/members/mypage/profile">
-                        <button className="btn btn-secondary btn-sm mt-2">내 프로필</button>
-                    </NavLink>
-                </div>
-                <hr/>
+                {/*<div className="text-center mb-3">*/}
+                {/*    {profilePicture ? (*/}
+                {/*        <Image*/}
+                {/*            src={profilePicture}*/}
+                {/*            roundedCircle*/}
+                {/*            className="mx-auto d-block mb-3"*/}
+                {/*            style={{width: "130px", height: "130px"}}*/}
+                {/*        />*/}
+                {/*    ) : (*/}
+                {/*        <CgProfile style={{width: "130px", height: "130px"}} />*/}
+                {/*    )}*/}
+                {/*    <br/>*/}
+                {/*    <div className="fw-bold fs-6">*/}
+                {/*        {memberName}*/}
+                {/*    </div>*/}
+                {/*    <div className="text-muted">{memberEmail}</div>*/}
+                {/*    <NavLink to="/members/mypage/profile">*/}
+                {/*        <button className="btn btn-secondary btn-sm mt-2">내 프로필</button>*/}
+                {/*    </NavLink>*/}
+                {/*</div>*/}
+                {/*<hr/>*/}
 
                 {/* Navigation Links */}
+                <Nav.Link className="nav-link-custom-title mt-2">마이페이지</Nav.Link>
+                <Nav.Link as={Link} to="/members/mypage/profile" className="nav-link-custom">프로필 관리</Nav.Link>
+                <br/>
                 <Nav.Link className="nav-link-custom-title">초록불 챌린지</Nav.Link>
                 <Nav.Link eventKey="myChallenges" className="nav-link-custom">나의 챌린지</Nav.Link>
                 <Nav.Link eventKey="myCertifications" className="nav-link-custom">나의 인증</Nav.Link>
                 <br/>
                 <Nav.Link className="nav-link-custom-title">반딧불이 스토어</Nav.Link>
-                <NavLink eventKey="orders" className="nav-link-custom" to="/members/mypage/orders">주문∙배송</NavLink>
+                <Nav.Link as={Link} className="nav-link-custom" to="/members/mypage/orders">주문∙배송</Nav.Link>
                 <Nav.Link eventKey="coupons" className="nav-link-custom">쿠폰</Nav.Link>
                 <Nav.Link eventKey="wishlist" className="nav-link-custom">찜한 상품</Nav.Link>
                 <Nav.Link eventKey="reviews" className="nav-link-custom">내가 쓴 리뷰</Nav.Link>
