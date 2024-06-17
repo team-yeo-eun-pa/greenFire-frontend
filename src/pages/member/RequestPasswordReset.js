261322951cfa2b5a2a11888893541af6144f3f29
siso <116954896+sisology@users.noreply.github.com>
2024-06-17 16:41:08 +0900

    feat: 비밀번호 찾기 기능 구현

--- src/pages/member/RequestPasswordReset.js
+++ src/pages/member/RequestPasswordReset.js
@@ -1,11 +1,18 @@
 import React, { useState } from 'react';
+import { Col, Form, InputGroup, Row, Button, Spinner } from "react-bootstrap";
 import axios from 'axios';
 
 const RequestPasswordReset = () => {
     const [email, setEmail] = useState('');
+    const [loading, setLoading] = useState(false);
+    const [success, setSuccess] = useState(false);
+    const [error, setError] = useState(null);
 
     const handleSubmit = async (event) => {
         event.preventDefault();
+        setLoading(true);
+        setError(null);
+        setSuccess(false);
 
         try {
             const response = await axios.post('http://localhost:8001/members/find-pwd', {
@@ -13,27 +20,56 @@ const RequestPasswordReset = () => {
             });
 
             if (response.status === 201) {
-                alert('Password reset email sent. Please check your inbox.');
+                setSuccess(true);
             }
         } catch (error) {
             console.error('Failed to request password reset', error);
-            alert('Failed to request password reset');
+            setError('존재하는 회원을 찾을 수 없습니다. 입력하신 값을 다시 확인해주세요.');
+        } finally {
+            setLoading(false);
         }
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <div>
-                <label>Email:</label>
-                <input
-                    type="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    required
-                />
-            </div>
-            <button type="submit">Request Password Reset</button>
-        </form>
+        <Row className="mt-5 justify-content-md-center">
+            <Col className="col-8">
+                <div className="fs-4 fw-semibold border-bottom border-2 border-dark-subtle p-2">비밀번호 찾기</div>
+                <Form className="p-5" onSubmit={handleSubmit}>
+                    <Form.Group as={Row} className="mb-3" controlId="formEmail">
+                        <Form.Label column sm="2">이메일</Form.Label>
+                        <Col sm="10">
+                            <InputGroup className="mb-3">
+                                <Form.Control
+                                    type="email"
+                                    placeholder="이메일을 입력해 주세요."
+                                    name="memberEmail"
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    required
+                                />
+                            </InputGroup>
+                        </Col>
+                    </Form.Group>
+
+                    <div className="px-0 d-flex align-items-center justify-content-center">
+                        <Button variant="success" type="submit" className="w-50" disabled={loading}>
+                            {loading ? <Spinner animation="border" size="sm" /> : "비밀번호 찾기"}
+                        </Button>
+                    </div>
+
+                    {success && (
+                        <div className="mt-3 text-center text-success">
+                            <h5>비밀번호 재설정 이메일이 전송되었습니다.</h5>
+                        </div>
+                    )}
+                    {error && (
+                        <div className="mt-3 text-center text-danger">
+                            <h5>{error}</h5>
+                        </div>
+                    )}
+                </Form>
+            </Col>
+        </Row>
     );
 };
 
--- src/pages/member/ResetPassword.js
+++ src/pages/member/ResetPassword.js
@@ -1,11 +1,16 @@
 import React, { useState } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import { Row, Col, Form, Button, Spinner } from 'react-bootstrap';
 
 const ResetPassword = () => {
     const [newPassword, setNewPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+    const [success, setSuccess] = useState(false);
     const location = useLocation();
+    const navigate = useNavigate();
 
     // URL 쿼리 파라미터로부터 멤버코드와 인증코드 추출
     const params = new URLSearchParams(location.search);
@@ -14,9 +19,13 @@ const ResetPassword = () => {
 
     const handleSubmit = async (event) => {
         event.preventDefault();
+        setLoading(true);
+        setError(null);
+        setSuccess(false);
 
         if (newPassword !== confirmPassword) {
-            alert('Passwords do not match');
+            setError('재입력하신 비밀번호가 일치하지 않습니다.');
+            setLoading(false);
             return;
         }
 
@@ -28,37 +37,86 @@ const ResetPassword = () => {
             });
 
             if (response.status === 204) {
-                alert('Password reset successful');
-                // 리다이렉트 또는 다른 동작 수행
+                setSuccess(true);
             }
         } catch (error) {
             console.error('Password reset failed', error);
-            alert('Password reset failed');
+            setError('Password reset failed');
+        } finally {
+            setLoading(false);
         }
     };
 
+    const handleMainClick = () => {
+        navigate("/");
+    };
+
+    const handleLoginClick = () => {
+        navigate("/members/login");
+    };
+
     return (
-        <form onSubmit={handleSubmit}>
-            <div>
-                <label>New Password:</label>
-                <input
-                    type="password"
-                    value={newPassword}
-                    onChange={(e) => setNewPassword(e.target.value)}
-                    required
-                />
-            </div>
-            <div>
-                <label>Confirm Password:</label>
-                <input
-                    type="password"
-                    value={confirmPassword}
-                    onChange={(e) => setConfirmPassword(e.target.value)}
-                    required
-                />
-            </div>
-            <button type="submit">Reset Password</button>
-        </form>
+        <Row className="mt-5 justify-content-md-center" style={{ paddingTop: 80 }}>
+            <Col className="col-8">
+                <div className="fs-4 fw-semibold border-bottom border-2 border-dark-subtle p-2">비밀번호 재설정</div>
+                <Form className="p-5" onSubmit={handleSubmit}>
+                    {!success && (
+                        <>
+                            <Form.Group as={Row} className="mb-3" controlId="formNewPassword">
+                                <Form.Label column sm="2">새 비밀번호</Form.Label>
+                                <Col sm="10">
+                                    <Form.Control
+                                        type="password"
+                                        placeholder="새 비밀번호를 입력해 주세요."
+                                        value={newPassword}
+                                        onChange={(e) => setNewPassword(e.target.value)}
+                                        required
+                                    />
+                                </Col>
+                            </Form.Group>
+
+                            <Form.Group as={Row} className="mb-3" controlId="formConfirmPassword">
+                                <Form.Label column sm="2">비밀번호 확인</Form.Label>
+                                <Col sm="10">
+                                    <Form.Control
+                                        type="password"
+                                        placeholder="비밀번호를 다시 입력해 주세요."
+                                        value={confirmPassword}
+                                        onChange={(e) => setConfirmPassword(e.target.value)}
+                                        required
+                                    />
+                                </Col>
+                            </Form.Group>
+
+                            <div className="px-0 d-flex align-items-center justify-content-center">
+                                <Button variant="success" type="submit" className="w-50" disabled={loading}>
+                                    {loading ? <Spinner animation="border" size="sm" /> : "비밀번호 재설정"}
+                                </Button>
+                            </div>
+                        </>
+                    )}
+
+                    {success && (
+                        <div className="mt-3 text-center text-success">
+                            <h5>비밀번호가 성공적으로 재설정되었습니다.</h5>
+                            <div className="mt-4 d-flex flex-column align-items-center">
+                                <Button className="mb-2 w-50" variant="outline-success" onClick={handleMainClick}>
+                                    메인으로
+                                </Button>
+                                <Button className="w-50" variant="success" onClick={handleLoginClick}>
+                                    로그인
+                                </Button>
+                            </div>
+                        </div>
+                    )}
+                    {error && (
+                        <div className="mt-3 text-center text-danger">
+                            <h5>{error}</h5>
+                        </div>
+                    )}
+                </Form>
+            </Col>
+        </Row>
     );
 };
 
